0239385894e96c1f82385fced6aafd539ea41f8b
Arun P. Mohanan <arun.p.m@linux.intel.com>
2020-05-11 13:17:43 +0530

    [MCTP]Add MCTP endpoint objects
    Add support to expose MCTP endpoint objects from json file.
    
    Tested: busctl calls shows interfaces and property values
    as per json file.
    root@intel-obmc:~# busctl tree xyz.openbmc_project.mctp-emulator
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/mctp
          └─/xyz/openbmc_project/mctp/device
            ├─/xyz/openbmc_project/mctp/device/1
            ├─/xyz/openbmc_project/mctp/device/2
            └─/xyz/openbmc_project/mctp/device/3
    root@intel-obmc:~# busctl introspect xyz.openbmc_project.mctp-emulator /xyz/openbmc_project/mctp/device/1
    NAME                                           TYPE      SIGNATURE RESULT/VALUE                             FLAGS
    org.freedesktop.DBus.Introspectable            interface -         -                                        -
    .Introspect                                    method    -         s                                        -
    org.freedesktop.DBus.Peer                      interface -         -                                        -
    .GetMachineId                                  method    -         s                                        -
    .Ping                                          method    -         -                                        -
    org.freedesktop.DBus.Properties                interface -         -                                        -
    .Get                                           method    ss        v                                        -
    .GetAll                                        method    s         a{sv}                                    -
    .Set                                           method    ssv       -                                        -
    .PropertiesChanged                             signal    sa{sv}as  -                                        -
    xyz.openbmc_project.MCTP.Endpoint              interface -         -                                        -
    .Mode                                          property  s         "busowner"                               emits-change
    .NetworkId                                     property  q         10                                       emits-change
    .Uuid                                          property  ay        16 101 110 100 112 111 105 110 116 117 … emits-change
    xyz.openbmc_project.MCTP.SupportedMessageTypes interface -         -                                        -
    .Ethernet                                      property  b         false                                    emits-change
    .MctpControl                                   property  b         true                                     emits-change
    .NCSI                                          property  b         false                                    emits-change
    .NVMeMgmtMsg                                   property  b         false                                    emits-change
    .PLDM                                          property  b         true                                     emits-change
    .SPDM                                          property  b         true                                     emits-change
    .VDIANA                                        property  b         false                                    emits-change
    .VDPCI                                         property  b         true                                     emits-change
    
    Change-Id: I157c2ef5c247bded4e5b6a50f9d320eff68c4a0b
    Signed-off-by: Arun P. Mohanan <arun.p.m@linux.intel.com>

--- mctp_emulator/src/main.cpp
+++ mctp_emulator/src/main.cpp
@@ -2,15 +2,125 @@
 #include "SMBusBinding.hpp"
 
 #include <CLI/CLI.hpp>
+#include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <phosphor-logging/log.hpp>
 #include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/bus.hpp>
+#include <xyz/openbmc_project/MCTP/Base/server.hpp>
+#include <xyz/openbmc_project/MCTP/Endpoint/server.hpp>
+#include <xyz/openbmc_project/MCTP/SupportedMessageTypes/server.hpp>
 
 std::map<std::string, binding> mctpBindingsMap = {{"smbus", binding::smbus},
                                                   {"pcie", binding::pcie}};
 
 std::shared_ptr<sdbusplus::asio::connection> bus;
+
+std::string endpointDataFile = "/usr/share/mctp-emulator/endpoints.json";
+std::string mctpDevObj = "/xyz/openbmc_project/mctp/device/";
+std::vector<std::shared_ptr<sdbusplus::asio::dbus_interface>> endpointInterface;
+std::vector<std::shared_ptr<sdbusplus::asio::dbus_interface>> msgTypeInterface;
+
+using json = nlohmann::json;
+using mctp_base = sdbusplus::xyz::openbmc_project::MCTP::server::Base;
+using mctp_endpoint = sdbusplus::xyz::openbmc_project::MCTP::server::Endpoint;
+
+void initEndPointDevices(
+    std::shared_ptr<sdbusplus::asio::object_server>& objectServer)
+{
+    std::ifstream jsonFile(endpointDataFile);
+    if (!jsonFile.good())
+    {
+        std::cerr << "unable to open " << endpointDataFile << "\n";
+    }
+
+    json endpoints = nullptr;
+    uint8_t eid;
+    std::string uuid;
+    std::string mode;
+    uint16_t networkId;
+    json msgType;
+    bool mctpControl;
+    bool pldm;
+    bool ncsi;
+    bool ethernet;
+    bool nvmeMgmtMsg;
+    bool spdm;
+    bool vdpci;
+    bool vdiana;
+
+    try
+    {
+        endpoints = json::parse(jsonFile, nullptr, false);
+    }
+    catch (json::exception& e)
+    {
+        std::cerr << "Error parsing " << endpointDataFile << "\n"
+                  << "message: " << e.what() << '\n'
+                  << "exception id: " << e.id << std::endl;
+        return;
+    }
+
+    for (auto iter : endpoints["Endpoints"])
+    {
+        try
+        {
+            eid = iter["Eid"];
+            uuid = iter["Uuid"];
+            mode = iter["Mode"];
+            networkId = iter["NetworkId"];
+            msgType = iter["SupportedMessageTypes"];
+            mctpControl = msgType["MctpControl"];
+            pldm = msgType["PLDM"];
+            ncsi = msgType["NCSI"];
+            ethernet = msgType["Ethernet"];
+            nvmeMgmtMsg = msgType["NVMeMgmtMsg"];
+            spdm = msgType["SPDM"];
+            vdpci = msgType["VDPCI"];
+            vdiana = msgType["VDIANA"];
+        }
+        catch (json::exception& e)
+        {
+            std::cerr << "message: " << e.what() << '\n'
+                      << "exception id: " << e.id << std::endl;
+            continue;
+        }
+
+        std::shared_ptr<sdbusplus::asio::dbus_interface> endpointIntf;
+        std::shared_ptr<sdbusplus::asio::dbus_interface> msgTypeIntf;
+        std::string mctpEpObj = mctpDevObj + std::to_string(eid);
+
+        auto enpointObjManager =
+            std::make_shared<sdbusplus::server::manager::manager>(
+                *bus, mctpEpObj.c_str());
+
+        endpointIntf =
+            objectServer->add_interface(mctpEpObj, mctp_endpoint::interface);
+        endpointIntf->register_property(
+            "Uuid", std::vector<uint8_t>(uuid.begin(), uuid.end()));
+        endpointIntf->register_property(
+            "Mode", mctp_base::convertBindingModeTypesToString(
+                        static_cast<mctp_base::BindingModeTypes>(1)));
+        endpointIntf->register_property("NetworkId", networkId);
+        endpointIntf->initialize();
+        endpointInterface.push_back(endpointIntf);
+
+        msgTypeIntf =
+            objectServer->add_interface(mctpEpObj, mctp_endpoint::interface);
+        msgTypeIntf->register_property("MctpControl", mctpControl);
+        msgTypeIntf->register_property("PLDM", pldm);
+        msgTypeIntf->register_property("NCSI", ncsi);
+        msgTypeIntf->register_property("Ethernet", ethernet);
+        msgTypeIntf->register_property("NVMeMgmtMsg", nvmeMgmtMsg);
+        msgTypeIntf->register_property("SPDM", spdm);
+        msgTypeIntf->register_property("VDPCI", vdpci);
+        msgTypeIntf->register_property("VDIANA", vdiana);
+        msgTypeIntf->initialize();
+        msgTypeInterface.push_back(msgTypeIntf);
+    }
+}
+
 int main()
 {
     // TODO: Read the binding configuration from a json file
@@ -49,7 +159,7 @@ int main()
             break;
         }
     }
-
+    initEndPointDevices(objectServer);
     ioc.run();
 
     return 0;
