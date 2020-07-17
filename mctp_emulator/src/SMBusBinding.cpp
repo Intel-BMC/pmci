1bc2bddcff7104cc9040e9b2688761b2d9b32099
Arun P. Mohanan <arun.p.m@linux.intel.com>
2020-07-17 23:23:04 +0530

    [mctp-emulator] Update bus number to use string
    The bus number used in mctpd is a string.
    Update emulator to use the same as that of mctpd.
    
    Tested: Below busctl call shows updated bus name as that of mctpd.
    busctl introspect xyz.openbmc_project.mctp-emulator /xyz/openbmc_project/mctp
    xyz.openbmc_project.MCTP.Binding.SMBus interface -          -                                        -
    .ArpMasterSupport                      property  b          false                                    emits-change
    .BusPath                               property  s          "/dev/i2c-2"                             emits-change
    
    Change-Id: Ie539d821ded2b68cacc5f6976ad327402bbfe4c0
    Signed-off-by: Arun P. Mohanan <arun.p.m@linux.intel.com>

--- mctp_emulator/src/SMBusBinding.cpp
+++ mctp_emulator/src/SMBusBinding.cpp
@@ -12,10 +12,10 @@ SMBusBinding::SMBusBinding(
         objServer->add_interface(objPath, smbusIntf.c_str());
 
     bool arpSupport = false;
-    uint8_t busNumber = 2;
+    std::string busNumber = "/dev/i2c-2";
     uint8_t slaveAddress = 0x21;
     smbusInterface->register_property("ArpMasterSupport", arpSupport);
-    smbusInterface->register_property("BusNumber", busNumber);
+    smbusInterface->register_property("BusPath", busNumber);
     smbusInterface->register_property("SlaveAddress", slaveAddress);
     smbusInterface->initialize();
 }
