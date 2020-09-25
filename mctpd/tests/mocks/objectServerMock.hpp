ec408f7a5cda07725317f1ad11cb7ddcb203ed0a
Ambrozewicz, Adrian <adrian.ambrozewicz@intel.com>
2020-09-25 14:55:24 +0200

    Cleanup sdbusplus mocks
    This change changes default behavior of sdbusplus mock to 'Nice',
    as by default test logs are polluted with warnings in case
    D-Bus api usage changes.
    
    Classes definition were rewritten to be more clean, consise and
    easily extendable.
    
    Addition of .gitignore as an extra, as working with unit tests
    requires building code locally, which generates few thousand new
    files.
    
    Tested:
    - ran the only unit test and it's still passing
    - no more logs about unexpected calls in console
    
    Change-Id: I1319a1e098537208247f6c29722523e00bd8476d
    Signed-off-by: Ambrozewicz, Adrian <adrian.ambrozewicz@intel.com>

--- mctpd/tests/mocks/objectServerMock.hpp
+++ mctpd/tests/mocks/objectServerMock.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <systemd/sd-bus.h>
 
 #include <iostream>
@@ -9,33 +11,46 @@
 namespace mctpd_mock
 {
 
+// Setting NiceMock as default as D-Bus calls tends to grow in the code and we
+// don't want our tests to be warning/failing everytime something changes
+//
+// Change to 'NaggyMock' if you want previous behavior (warning messages) in
+// your tests. It can be useful during developing tests.
+#define MockType ::testing::NiceMock
+
+namespace impl
+{
+
+// Mixin class used to generate proper function for type T
+template <typename T>
+struct register_property_mock
+{
+    MOCK_METHOD(bool, register_property,
+                (const std::string&, T, sdbusplus::asio::PropertyPermission&));
+    MOCK_METHOD(bool, register_property, (const std::string&, T));
+};
+
+template <typename... PropertyTypes>
 class dbus_interface_mock
+    : public MockType<register_property_mock<PropertyTypes>>...
 {
   public:
-    dbus_interface_mock(__attribute__((unused)) const std::string& path,
-                        __attribute__((unused)) const std::string& name){};
-    virtual ~dbus_interface_mock(){};
+    // Need those to properly extract mocked functions
+    using MockType<register_property_mock<PropertyTypes>>::register_property...;
+    using MockType<
+        register_property_mock<PropertyTypes>>::gmock_register_property...;
 
-    MOCK_METHOD3(register_property, bool(const std::string&, uint8_t,
-                                         sdbusplus::asio::PropertyPermission&));
-    MOCK_METHOD3(register_property, bool(const std::string&, uint16_t,
-                                         sdbusplus::asio::PropertyPermission&));
-    MOCK_METHOD3(register_property, bool(const std::string&, const std::string&,
-                                         sdbusplus::asio::PropertyPermission&));
-    MOCK_METHOD3(register_property, bool(const std::string&, bool,
-                                         sdbusplus::asio::PropertyPermission&));
-    MOCK_METHOD3(register_property,
-                 bool(const std::string&, std::vector<uint8_t>,
-                      sdbusplus::asio::PropertyPermission&));
-    MOCK_METHOD2(register_property, bool(const std::string&, uint8_t));
-    MOCK_METHOD2(register_property, bool(const std::string&, uint16_t));
-    MOCK_METHOD2(register_property,
-                 bool(const std::string&, const std::string&));
-    MOCK_METHOD2(register_property, bool(const std::string&, bool));
-    MOCK_METHOD2(register_property,
-                 bool(const std::string&, std::vector<uint8_t>));
-    MOCK_METHOD1(register_method, bool(const std::string&));
-    MOCK_METHOD1(register_signal, bool(const std::string&));
+    dbus_interface_mock(__attribute__((unused)) const std::string& path,
+                        __attribute__((unused)) const std::string& name)
+    {
+    }
+    virtual ~dbus_interface_mock()
+    {
+    }
+
+    MOCK_METHOD(bool, initialize, ());
+    MOCK_METHOD(bool, register_method, (const std::string&));
+    MOCK_METHOD(bool, register_signal, (const std::string&));
 
     template <typename... SignalSignature>
     bool register_signal(const std::string& name)
@@ -50,20 +65,41 @@ class dbus_interface_mock
         return register_method(name);
     }
 
-    MOCK_METHOD(bool, initialize, ());
+    void returnByDefault(const bool expectedReturn)
+    {
+        using ::testing::_;
+        using ::testing::An;
+        using ::testing::Return;
+
+        ON_CALL(*this, initialize).WillByDefault(Return(expectedReturn));
+        ON_CALL(*this, register_signal).WillByDefault(Return(expectedReturn));
+        ON_CALL(*this, register_method).WillByDefault(Return(expectedReturn));
+
+        // Applies return for each type in PropertyTypes list
+        (..., ON_CALL(*this, register_property(_, An<PropertyTypes>()))
+                  .WillByDefault(Return(expectedReturn)));
+        (..., ON_CALL(*this, register_property(
+                                 _, An<PropertyTypes>(),
+                                 An<sdbusplus::asio::PropertyPermission&>()))
+                  .WillByDefault(Return(expectedReturn)));
+    }
 };
 
+template <typename dbus_interface_mock>
 class object_server_mock
 {
   public:
     const std::string mctpIntf = "xyz.openbmc_project.mctp.mock.base";
     std::shared_ptr<dbus_interface_mock> dbusIfMock;
 
-    object_server_mock(const std::string& path)
+    object_server_mock(const std::string& path) :
+        dbusIfMock(std::make_shared<dbus_interface_mock>(path, mctpIntf))
     {
-        dbusIfMock = std::make_shared<dbus_interface_mock>(path, mctpIntf);
-    };
-    virtual ~object_server_mock(){};
+    }
+
+    virtual ~object_server_mock()
+    {
+    }
 
     std::shared_ptr<dbus_interface_mock> add_interface(__attribute__((unused))
                                                        const std::string& path,
@@ -77,7 +113,16 @@ class object_server_mock
                           std::shared_ptr<dbus_interface_mock>& iface)
     {
         return true;
-    };
+    }
 };
+
+} // namespace impl
+
+// DBus interface with list of property types supported
+using dbus_interface_mock = MockType<impl::dbus_interface_mock<
+    bool, uint8_t, uint16_t, const std::string&, std::vector<uint8_t>>>;
+
+using object_server_mock =
+    MockType<impl::object_server_mock<dbus_interface_mock>>;
 
 } // namespace mctpd_mock
