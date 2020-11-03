beb9aa8d4d9c8cec6a39ef4fe541a1e6cbca523a
Sandino Flores <sandino.flores.moreno@intel.com>
2020-11-02 20:01:07 -0800

    Add atomic send-receive D-Bus method
    Send-Receive MCTP API provides options for upper application to get
    the response for their request in the same D-Bus API, rather than
    separately monitoring the D-Bus signal for the response.
    
    Signature of this method is : yayq , y - the first byte represents
    destination EID, ay - is the payload packet, q - is a uint16_t timeout
    duration; this is the time duration for which MCTP shall track the
    request packets.
    
    If no response can be sent within the timeout, then the method call
    throws the standard timeout exception.
    
    Tested:
      # Multiple messages sent simultaneously
      $ for((i=0; i<32; i++)); do
          busctl call
                 xyz.openbmc_project.MCTP-WC_Baseboard_2fMCTP_SMBus_PCIe_slot \
                 /xyz/openbmc_project/mctp xyz.openbmc_project.MCTP.Base \
                 SendReceiveMctpMessagePayload \
                 yayq 19 4 1 0x81 0 4 100 &
      done
    
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
      ay 13 1 1 0 4 0 53 0 0 0 0 0 0 0
    
      # Existing method works unaffected
      $ busctl call \
             xyz.openbmc_project.MCTP-WC_Baseboard_2fMCTP_SMBus_PCIe_slot \
             /xyz/openbmc_project/mctp \
             xyz.openbmc_project.MCTP.Base \
             SendMctpMessagePayload \
             yybay 19 0 0 4 1 0x81 0 4
    
      $ busctl --match "type='signal',path_namespace='/xyz/openbmc_project/mctp'" monitor
      Monitoring bus message stream.
      Type=signal  Endian=l  Flags=1  Version=1 Cookie=352
      Sender=:1.7677  Path=/xyz/openbmc_project/mctp  Interface=xyz.openbmc_project.MCTP.Base  Member=MessageReceivedSignal
      UniqueName=:1.7677
      MESSAGE "yyybay" {
              BYTE 1;
              BYTE 19;
              BYTE 0;
              BOOLEAN false;
              ARRAY "y" {
                      BYTE 1;
                      BYTE 1;
                      BYTE 0;
                      BYTE 4;
                      BYTE 0;
                      BYTE 53;
                      BYTE 0;
                      BYTE 0;
                      BYTE 0;
                      BYTE 0;
                      BYTE 0;
                      BYTE 0;
                      BYTE 0;
              };
      };
    
    Change-Id: I7f032ceb3ec560e4dea41f8287bef083cd35c9cf
    Signed-off-by: Sandino Flores <sandino.flores.moreno@intel.com>

--- mctpd/include/MCTPBinding.hpp
+++ mctpd/include/MCTPBinding.hpp
@@ -102,6 +102,56 @@ using ConfigurationVariant =
 
 extern std::shared_ptr<sdbusplus::asio::connection> conn;
 
+class MctpTransmissionQueue
+{
+  public:
+    struct Message
+    {
+        Message(size_t index_, std::vector<uint8_t>&& payload_,
+                std::vector<uint8_t>&& privateData_,
+                boost::asio::io_context& ioc);
+
+        size_t index{0};
+        std::optional<uint8_t> tag;
+        std::vector<uint8_t> payload{};
+        std::vector<uint8_t> privateData{};
+        boost::asio::steady_timer timer;
+        std::optional<std::vector<uint8_t>> response{};
+    };
+
+    std::shared_ptr<Message> transmit(struct mctp* mctp, mctp_eid_t destEid,
+                                      std::vector<uint8_t>&& payload,
+                                      std::vector<uint8_t>&& privateData,
+                                      boost::asio::io_context& ioc);
+
+    bool receive(struct mctp* mctp, mctp_eid_t srcEid, uint8_t msgTag,
+                 std::vector<uint8_t>&& response, boost::asio::io_context& ioc);
+
+    void dispose(mctp_eid_t destEid, const std::shared_ptr<Message>& message);
+
+  private:
+    struct Tags
+    {
+        std::optional<uint8_t> next() const;
+        void emplace(uint8_t flag);
+        void erase(uint8_t flag);
+
+        uint8_t bits{0xff};
+    };
+
+    struct Endpoint
+    {
+        Tags availableTags;
+        std::map<uint8_t, std::shared_ptr<Message>> transmittedMessages{};
+        std::map<size_t, std::shared_ptr<Message>> queuedMessages{};
+
+        size_t msgCounter{0u};
+        void transmitQueuedMessages(struct mctp* mctp, mctp_eid_t destEid);
+    };
+
+    std::map<mctp_eid_t, Endpoint> endpoints{};
+};
+
 class MctpBinding
 {
   public:
@@ -124,6 +174,7 @@ class MctpBinding
     struct mctp* mctp = nullptr;
     uint8_t ownEid;
     uint8_t busOwnerEid;
+    MctpTransmissionQueue transmissionQueue;
 
     void initializeMctp();
     void initializeLogging(void);
--- mctpd/src/MCTPBinding.cpp
+++ mctpd/src/MCTPBinding.cpp
@@ -26,6 +26,133 @@ static uint8_t getInstanceId(const uint8_t msg)
     return msg & MCTP_CTRL_HDR_INSTANCE_ID_MASK;
 }
 
+MctpTransmissionQueue::Message::Message(size_t index_,
+                                        std::vector<uint8_t>&& payload_,
+                                        std::vector<uint8_t>&& privateData_,
+                                        boost::asio::io_context& ioc) :
+    index(index_),
+    payload(std::move(payload_)), privateData(std::move(privateData_)),
+    timer(ioc)
+{
+}
+
+std::optional<uint8_t> MctpTransmissionQueue::Tags::next() const
+{
+    if (!bits)
+    {
+        return std::nullopt;
+    }
+    return static_cast<uint8_t>(__builtin_ctz(bits));
+}
+
+void MctpTransmissionQueue::Tags::emplace(uint8_t flag)
+{
+    bits |= static_cast<uint8_t>(1 << flag);
+}
+
+void MctpTransmissionQueue::Tags::erase(uint8_t flag)
+{
+    bits &= static_cast<uint8_t>(~(1 << flag));
+}
+
+std::shared_ptr<MctpTransmissionQueue::Message> MctpTransmissionQueue::transmit(
+    struct mctp* mctp, mctp_eid_t destEid, std::vector<uint8_t>&& payload,
+    std::vector<uint8_t>&& privateData, boost::asio::io_context& ioc)
+{
+    auto& endpoint = endpoints[destEid];
+    auto msgIndex = endpoint.msgCounter++;
+    auto message = std::make_shared<Message>(msgIndex, std::move(payload),
+                                             std::move(privateData), ioc);
+    endpoint.queuedMessages.emplace(msgIndex, message);
+    endpoint.transmitQueuedMessages(mctp, destEid);
+    return message;
+}
+
+void MctpTransmissionQueue::Endpoint::transmitQueuedMessages(struct mctp* mctp,
+                                                             mctp_eid_t destEid)
+{
+    while (!queuedMessages.empty())
+    {
+        const std::optional<uint8_t> nextTag = availableTags.next();
+        if (!nextTag)
+        {
+            break;
+        }
+        auto msgTag = nextTag.value();
+        auto queuedMessageIter = queuedMessages.begin();
+        auto message = std::move(queuedMessageIter->second);
+        queuedMessages.erase(queuedMessageIter);
+
+        int rc = mctp_message_tx(mctp, destEid, message->payload.data(),
+                                 message->payload.size(), true, msgTag,
+                                 message->privateData.data());
+        if (rc < 0)
+        {
+            phosphor::logging::log<phosphor::logging::level::ERR>(
+                "Error in mctp_message_tx");
+            continue;
+        }
+
+        availableTags.erase(msgTag);
+        message->tag = msgTag;
+        transmittedMessages.emplace(msgTag, std::move(message));
+    }
+}
+
+bool MctpTransmissionQueue::receive(struct mctp* mctp, mctp_eid_t srcEid,
+                                    uint8_t msgTag,
+                                    std::vector<uint8_t>&& response,
+                                    boost::asio::io_context& ioc)
+{
+    auto endpointIter = endpoints.find(srcEid);
+    if (endpointIter == endpoints.end())
+    {
+        return false;
+    }
+
+    auto& endpoint = endpointIter->second;
+    auto messageIter = endpoint.transmittedMessages.find(msgTag);
+    if (messageIter == endpoint.transmittedMessages.end())
+    {
+        return false;
+    }
+
+    const auto message = messageIter->second;
+    message->response = std::move(response);
+    endpoint.transmittedMessages.erase(messageIter);
+    message->tag.reset();
+    endpoint.availableTags.emplace(msgTag);
+
+    // Now that another tag is available, try to transmit any queued messages
+    message->timer.cancel();
+    ioc.post([this, mctp, srcEid] {
+        endpoints[srcEid].transmitQueuedMessages(mctp, srcEid);
+    });
+    return true;
+}
+
+void MctpTransmissionQueue::dispose(mctp_eid_t destEid,
+                                    const std::shared_ptr<Message>& message)
+{
+    auto& endpoint = endpoints[destEid];
+    auto queuedMessageIter = endpoint.queuedMessages.find(message->index);
+    if (queuedMessageIter != endpoint.queuedMessages.end())
+    {
+        endpoint.queuedMessages.erase(queuedMessageIter);
+    }
+    if (message->tag)
+    {
+        auto msgTag = message->tag.value();
+        endpoint.availableTags.emplace(msgTag);
+
+        auto transmittedMessageIter = endpoint.transmittedMessages.find(msgTag);
+        if (transmittedMessageIter != endpoint.transmittedMessages.end())
+        {
+            endpoint.transmittedMessages.erase(transmittedMessageIter);
+        }
+    }
+}
+
 void MctpBinding::handleCtrlResp(void* msg, const size_t len)
 {
     mctp_ctrl_msg_hdr* respHeader = reinterpret_cast<mctp_ctrl_msg_hdr*>(msg);
@@ -84,23 +211,32 @@ void MctpBinding::rxMessage(uint8_t srcEid, void* data, void* msg, size_t len,
 
     response.assign(payload, payload + len);
 
+    auto& binding = *static_cast<MctpBinding*>(data);
+
     if (msgType != MCTP_MESSAGE_TYPE_MCTP_CTRL)
     {
+        if (!tagOwner &&
+            binding.transmissionQueue.receive(binding.mctp, srcEid, msgTag,
+                                              std::move(response), binding.io))
+        {
+            return;
+        }
+
         auto msgSignal =
             conn->new_signal("/xyz/openbmc_project/mctp",
                              mctp_server::interface, "MessageReceivedSignal");
         msgSignal.append(msgType, srcEid, msgTag, tagOwner, response);
         msgSignal.signal_send();
+        return;
     }
 
-    auto* binding = static_cast<MctpBinding*>(data);
     // TODO: Take into account the msgTags too when we verify control messages.
-    if (mctp_is_mctp_ctrl_msg(msg, len) && !mctp_ctrl_msg_is_req(msg, len) &&
-        !tagOwner)
+    if (!tagOwner && mctp_is_mctp_ctrl_msg(msg, len) &&
+        !mctp_ctrl_msg_is_req(msg, len))
     {
         phosphor::logging::log<phosphor::logging::level::INFO>(
             "MCTP Control packet response received!!");
-        binding->handleCtrlResp(msg, len);
+        binding.handleCtrlResp(msg, len);
     }
 }
 
@@ -125,8 +261,8 @@ void MctpBinding::handleMCTPControlRequests(uint8_t srcEid, void* data,
             "MCTP Control Message expects that tagOwner is set");
         return;
     }
-    auto* binding = static_cast<MctpBinding*>(data);
-    binding->handleCtrlReq(srcEid, bindingPrivate, msg, len, msgTag);
+    auto& binding = *static_cast<MctpBinding*>(data);
+    binding.handleCtrlReq(srcEid, bindingPrivate, msg, len, msgTag);
 }
 
 std::optional<std::vector<uint8_t>>
@@ -224,6 +360,64 @@ MctpBinding::MctpBinding(std::shared_ptr<object_server>& objServer,
                 phosphor::logging::log<phosphor::logging::level::ERR>(
                     "Invalid destination EID");
                 return -1;
+            });
+
+        mctpInterface->register_method(
+            "SendReceiveMctpMessagePayload",
+            [this](boost::asio::yield_context yield, uint8_t dstEid,
+                   std::vector<uint8_t> payload,
+                   uint16_t timeout) -> std::vector<uint8_t> {
+                uint8_t msgType = payload[0]; // Always the first byte
+                if (msgType == MCTP_MESSAGE_TYPE_MCTP_CTRL)
+                {
+                    phosphor::logging::log<phosphor::logging::level::ERR>(
+                        "Cannot transmit control messages");
+                    throw std::system_error(
+                        std::make_error_code(std::errc::invalid_argument));
+                }
+
+                std::optional<std::vector<uint8_t>> pvtData =
+                    getBindingPrivateData(dstEid);
+                if (!pvtData)
+                {
+                    phosphor::logging::log<phosphor::logging::level::ERR>(
+                        "Invalid destination EID");
+                    throw std::system_error(
+                        std::make_error_code(std::errc::invalid_argument));
+                }
+
+                boost::system::error_code ec;
+                auto message =
+                    transmissionQueue.transmit(mctp, dstEid, std::move(payload),
+                                               std::move(pvtData).value(), io);
+
+                message->timer.expires_after(
+                    std::chrono::milliseconds(timeout));
+                message->timer.async_wait(yield[ec]);
+
+                if (ec && ec != boost::asio::error::operation_aborted)
+                {
+                    transmissionQueue.dispose(dstEid, message);
+                    phosphor::logging::log<phosphor::logging::level::ERR>(
+                        "Timer failed");
+                    throw std::system_error(
+                        std::make_error_code(std::errc::connection_aborted));
+                }
+                if (!message->response)
+                {
+                    phosphor::logging::log<phosphor::logging::level::ERR>(
+                        "No response");
+                    throw std::system_error(
+                        std::make_error_code(std::errc::timed_out));
+                }
+                if (message->response->empty())
+                {
+                    phosphor::logging::log<phosphor::logging::level::ERR>(
+                        "Empty response");
+                    throw std::system_error(
+                        std::make_error_code(std::errc::no_message_available));
+                }
+                return std::move(message->response).value();
             });
 
         mctpInterface->register_signal<uint8_t, uint8_t, uint8_t, bool,
